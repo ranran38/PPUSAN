2363f49a3db2f4d5cc1469fbe8516003f289526f
nanaakuma <dk4147@naver.com>
2022-09-13 17:19:01 +0900

    weather

---
+++ ppusan/src/main/java/com/project/ppusan/domain/Clothes.java
@@ -0,0 +1,13 @@
+package com.project.ppusan.domain;
+
+import lombok.Data;
+
+@Data
+public class Clothes {
+	private int clothesId;
+	private Double maxT;
+	private Double minT;
+	private String solution;
+	private String path;
+
+}
