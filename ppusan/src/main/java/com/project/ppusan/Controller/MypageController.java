6b67bf20d7e595efa3a4f1dce6e83c21e3418d21
nanaakuma <dk4147@naver.com>
2022-09-28 17:18:10 +0900

    mypage

--- ppusan/src/main/java/com/project/ppusan/Controller/MypageController.java
+++ ppusan/src/main/java/com/project/ppusan/Controller/MypageController.java
@@ -1,5 +1,8 @@
 package com.project.ppusan.Controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +13,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.ppusan.domain.Member;
 import com.project.ppusan.domain.UpdateMemberForm;
+import com.project.ppusan.security.UserInfo;
 import com.project.ppusan.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,12 +30,38 @@ import lombok.extern.slf4j.Slf4j;
 public class MypageController {
 	
 	private final MemberService memberService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	@GetMapping("/update") 
-	public String update() {
-		return "mypage/updateForm"; 
+	/*
+	 * @GetMapping("/update") public String update() { return "mypage/updateForm"; }
+	 */
+	
+	@GetMapping("/update")
+	public String getMember(@AuthenticationPrincipal UserInfo user, Model model) {
+		String memberId = user.getUsername();
+		Member member = memberService.findMember(memberId);
+		model.addAttribute("updateForm", member.toUpdateMemberForm());
+		return "/mypage/updateForm";
 	}
 	
+	@PostMapping("/update/{memberId}")
+	public String updateMember(@PathVariable String memberId, 
+							   @Validated @ModelAttribute UpdateMemberForm updateMemberForm, 
+							   BindingResult bindingResult, 
+							   Model model) {
+		log.info("upateForm : {}", updateMemberForm);
+		// Form Validation
+		if (bindingResult.hasErrors()) {
+			return "/mypage/updateForm";
+		}
+	
+		if (updateMemberForm.getPassword() == null || updateMemberForm.getPassword() == "") {
+			updateMemberForm.setPassword(null);
+		}
+		updateMemberForm.setPassword(bCryptPasswordEncoder.encode(updateMemberForm.getPassword()));
+		memberService.updateMember(updateMemberForm.toMember());
+		return "redirect:/";
+	}
 	
 	/*
 	 * @GetMapping(value = "/update/{memberId}") public String
@@ -48,6 +79,11 @@ public class MypageController {
 	@GetMapping("/planner")
 	public String planner() {
 		return "/mypage/planner";
+	}
+	
+	@GetMapping("/plannerwrite")
+	public String plannerwrite() {
+		return "/mypage/plannerwrite";
 	}
 	
 	@GetMapping("/plannerDetail")
